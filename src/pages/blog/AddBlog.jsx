d547fe2428d0b2ba8f9a6b8db6b0944d7dd7ee9b
Birat <thapabirat95@gmail.com>
2024-10-16 15:02:36 +0545

    register,login,addblog api integration

--- src/pages/blog/AddBlog.jsx
+++ src/pages/blog/AddBlog.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
 import Form from './components/form/Form'
+import axios from 'axios'
+import { baseUrl } from '../../../config'
 
 const AddBlog = () => {
+  const handleCreateBlog =async (data)=>{
+   
+      const respone= await axios.post(`${baseUrl}/blog`,data,{
+        headers:{
+          "Content-Type" : "multipart/form-data",
+          "Authorization": localStorage.getItem('token')
+        }
+      })
+     
+
+  }
   return (
     <Layout>
       
-		<Form  type='Create'/>
+		<Form  type='Create' onSubmit={handleCreateBlog}/>
 
     </Layout>
   )
--- src/pages/blog/components/form/Form.jsx
+++ src/pages/blog/components/form/Form.jsx
@@ -1,27 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Form = ({type}) => {
+const Form = ({type,onSubmit}) => {
+const [data,setData] =useState({
+  title:'',
+  subtitle:'',
+  description:'',
+  category:'',
+  image:''
+})
+
+const handleChange = (e)=>{
+  const{name,value}=e.target
+  setData({
+    ...data,
+    [name]:name ==='image'?e.target.files[0]:value
+  })
+}
+
+const handleSubmit =(e)=>{
+  e.preventDefault()
+  onSubmit(data)
+}
   return (
     <div className="flex justify-center items-center w-screen h-screen ">
 	
     <div className="container mx-auto my-4 px-4 lg:px-20">
   
+     
       <div className="w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl mx-25">
         <div className="flex">
           <h1 className="font-bold uppercase text-5xl">{type} <br /> Blog</h1>
         </div>
+
+        <form onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
           <input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-              type="text" placeholder="Title*" />
+              type="text" placeholder="Title*"  name="title" onChange={handleChange}  required/>
           <input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-              type="text" placeholder="Subtitle" />
+              type="text" placeholder="Subtitle" name="subtitle"  onChange={handleChange}  required/>
           <input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-              type="file"  />
+              type="file"  name="image"  onChange={handleChange} />
           <input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-              type="number" placeholder="Category*" />
+              type="number" placeholder="Category*" name="category"  onChange={handleChange} required/>
           </div>
           <div className="my-4">
-            <textarea placeholder="Description*" className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
+            <textarea placeholder="Description*" className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"  name="description" onChange={handleChange} required></textarea>
           </div>
           <div className="my-2 w-1/2 lg:w-1/4">
             <button className="uppercase text-sm font-bold tracking-wide bg-blue-900 text-gray-100 p-3 rounded-lg w-full 
@@ -29,9 +52,9 @@ const Form = ({type}) => {
               Send Message
             </button>
           </div>
+          </form>
         </div>
-  
-        
+     
       </div>
       
   </div>
